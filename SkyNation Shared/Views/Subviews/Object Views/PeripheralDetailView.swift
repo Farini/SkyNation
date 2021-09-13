109c1be6005656e29f307d41a852d2aa56c64696
Carlos Farini <cfarini@gmail.com>
2021-09-12 20:45:34 -0400

    missing peripheral detail view

--- SkyNation Shared/Views/Subviews/Object Views/PeripheralDetailView.swift
+++ SkyNation Shared/Views/Subviews/Object Views/PeripheralDetailView.swift
@@ -7,124 +7,6 @@
 
 import SwiftUI
 
-struct PeripheralDetailView: View {
-    
-//    @ObservedObject var controller:LSSModel
-    var delegator:LSSDelegate
-    @State var peripheral:PeripheralObject
-    
-    var body: some View {
-        VStack {
-            
-            // Presentation
-            Text("\(peripheral.peripheral.rawValue)")
-                .font(.title)
-            
-            if let _ = peripheral.getImage() {
-                peripheral.getImage()
-                    .fixedSize()
-                    .frame(width: 64, height: 64, alignment: .center)
-            } else {
-                Image(systemName: "questionmark")
-                    .fixedSize()
-                    .frame(width: 64, height: 64, alignment: .center)
-            }
-            
-            Text(peripheral.peripheral.describer)
-                .foregroundColor(.gray)
-                .frame(width: 300, alignment: .center)
-            
-            Divider()
-            
-            // Conditions
-            Group {
-                Text("Breakable: \(peripheral.breakable ? "YES":"NO")")
-                    .foregroundColor(peripheral.breakable ? Color.orange:Color.green)
-                
-                Text("Broken: \(peripheral.isBroken ? "YES":"NO")")
-                    .foregroundColor(peripheral.isBroken ? Color.red:Color.green)
-                
-                Text("Power: \(peripheral.powerOn ? "On":"Off")")
-                    .foregroundColor(peripheral.powerOn ? Color.green:Color.orange)
-                
-                // Time since fixed
-                if let d = peripheral.lastFixed?.timeIntervalSince(Date()) {
-                    Text("Time (let d): \(d) s")
-                }
-            }
-            
-            Divider()
-            
-            if !peripheral.peripheral.instantUse.isEmpty {
-                Group {
-                    Text("For 100 energy, this Peripheral can be used intantly to perform the following operation:")
-                        .frame(width: 300, alignment: .center)
-                        .foregroundColor(.gray)
-                        .padding([.bottom], 6)
-                    
-                    Text(peripheral.peripheral.instantUse)
-                        .frame(width: 300, alignment: .center)
-                        .padding([.bottom], 6)
-                    
-                    
-//                    ForEach(delegator.peripheralMessages, id:\.self) { msg in
-//                        Text(msg)
-//                            .foregroundColor(.green)
-//                            .frame(width: 300, alignment: .center)
-//                    }
-                    
-//                    ForEach(controller.peripheralMessages, id:\.self) { msg in
-//                        Text(msg)
-//                            .foregroundColor(.green)
-//                            .frame(width: 300, alignment: .center)
-//                    }
-                    
-//                    ForEach(controller.peripheralIssues, id:\.self) { msg in
-//                        Text(msg)
-//                            .foregroundColor(.red)
-//                            .frame(width: 300, alignment: .center)
-//                    }
-                    
-                    Button("Instant Use") {
-//                        print("Instause!")
-//                        controller.instantUse(peripheral: peripheral)
-                        delegator.instantUse(peripheral: peripheral)
-                    }
-                    .buttonStyle(NeumorphicButtonStyle(bgColor:.orange))
-                    
-                    Divider()
-                }
-            }
-            
-            // Buttons
-            HStack {
-                Button("Fix") {
-//                    peripheral.isBroken.toggle()
-//                    peripheral.lastFixed = Date()
-//                    controller.fixBroken(peripheral: peripheral)
-                    delegator.fixBroken(peripheral: peripheral)
-                }
-                .buttonStyle(NeumorphicButtonStyle(bgColor:.orange))
-                .disabled(!peripheral.isBroken)
-                
-                Button("Break") {
-                    peripheral.isBroken.toggle()
-                }
-                .buttonStyle(NeumorphicButtonStyle(bgColor:.orange))
-                .disabled(peripheral.isBroken)
-                
-                Button(peripheral.powerOn ? "Power off":"Power on") {
-                    peripheral.powerOn.toggle()
-                }
-                .buttonStyle(NeumorphicButtonStyle(bgColor:.orange))
-                .disabled(peripheral.isBroken)
-//                Toggle("Power", isOn: $peripheral.powerOn)
-            }
-            .padding()
-        }
-    }
-}
-
 struct PeripheralSmallView: View {
     
     @State var peripheral:PeripheralObject
@@ -138,20 +20,9 @@ struct PeripheralSmallView: View {
     
 }
 
-struct PeripheralDetailView_Previews: PreviewProvider {
-    
-    static var peripherals:[PeripheralObject] = [
-        PeripheralObject(peripheral: .Electrolizer),
-        PeripheralObject(peripheral: .Antenna),
-        PeripheralObject(peripheral: .Condensator),
-        PeripheralObject(peripheral: .ScrubberCO2),
-        PeripheralObject(peripheral: .Radiator) ]
+struct Peripheral_Previews: PreviewProvider {
     
     static var previews: some View {
-        ForEach(peripherals, id:\.id) { peri in
-//            PeripheralDetailView(controller:LSSModel(), peripheral: peri)
-            PeripheralDetailView(delegator: LSSModel(), peripheral: peri)
-        }
-        
+        PeripheralSmallView(peripheral: PeripheralObject(peripheral: .Electrolizer))
     }
 }
